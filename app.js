95af708d6367aabf0f142480eab4e6ecf672f2e0
Alan Sanchez <alnwolff@gmail.com>
2021-11-22 19:22:08 +0100

    add app, index and style files and some tags

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.querySelector('#score')
+    const resultDisplay = document.querySelector('#result')
+    
+    // create a playing board
+})
