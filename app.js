d8e478ef5b8fa203e6009f2bc2d0c444cdbc0711
Alan Sanchez <alnwolff@gmail.com>
2021-11-22 20:08:40 +0100

    add generate function to add number 2 randomly

--- app.js
+++ app.js
@@ -13,11 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
       gridDisplay.appendChild(square);
       squares.push(square);
     }
+    generate()
+    generate()
   }
-  createBoard()
+  createBoard();
 
   // generate a number randomly
 
-  
-
+  function generate() {
+    randomNumber = Math.floor(Math.random() * squares.length);
+    if (squares[randomNumber].innerHTML == 0) {
+        squares[randomNumber].innerHTML = 2
+    } else generate();
+  }
 });
