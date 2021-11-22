533b4efba04cf1fd7e930e358c81d41bd6da4f07
Alan Sanchez <alnwolff@gmail.com>
2021-11-22 19:37:10 +0100

    create the board and add the squares on it

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const gridDisplay = document.querySelector('.grid')
-    const scoreDisplay = document.querySelector('#score')
-    const resultDisplay = document.querySelector('#result')
-    
-    // create a playing board
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const gridDisplay = document.querySelector(".grid");
+  const scoreDisplay = document.querySelector("#score");
+  const resultDisplay = document.querySelector("#result");
+  const width = 4;
+  let squares = [];
+  // create a playing board
+
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      square = document.createElement("div");
+      square.innerHTML = 0;
+      gridDisplay.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard()
+
+  // generate a number randomly
+
+  
+
+});
